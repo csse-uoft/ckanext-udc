af8afbd7619cc6b9441f04557e3bf3c8b3a39ded
LesterLyu <lvds2000@gmail.com>
2023-10-16 14:33:04 -0400

    Fix typo

--- ckanext/udc_import/plugin.py
+++ ckanext/udc_import/plugin.py
@@ -4,6 +4,7 @@ from ckan.types import Action, AuthFunction, Context
 import ckanext.udc_import.logic.actions as action
 import ckanext.udc_import.views as views
 import ckanext.udc_import.auth as auth
+from typing import Dict
 
 
 class UdcImportPlugin(plugins.SingletonPlugin):
@@ -19,7 +20,7 @@ class UdcImportPlugin(plugins.SingletonPlugin):
         toolkit.add_resource("assets", "udc_import")
 
     # IActions
-    def get_actions(self) -> dict[str, Action]:
+    def get_actions(self) -> Dict[str, Action]:
         return {
             'udc_import_submit': action.udc_import_submit,
             'udc_import_check_permission': action.check_permission,
@@ -31,7 +32,7 @@ class UdcImportPlugin(plugins.SingletonPlugin):
         return views.get_blueprints()
     
     # IAuthFunctions
-    def get_auth_functions(self) -> dict[str, AuthFunction]:
+    def get_auth_functions(self) -> Dict[str, AuthFunction]:
         return {
             u'udc_import_view': auth.udc_import_auth,
             u'udc_import_submit': auth.udc_import_auth,
