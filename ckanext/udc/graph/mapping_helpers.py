54c3a6ad86f8f4edc24d3b5161985dc8a4dfa9b1
LesterLyu <lvds2000@gmail.com>
2023-11-02 11:33:11 -0400

    Add License Mapping. & Update config.

--- ckanext/udc/graph/mapping_helpers.py
+++ ckanext/udc/graph/mapping_helpers.py
@@ -4,8 +4,10 @@ import uuid
 from datetime import datetime
 from rdflib import Literal, XSD
 from .contants import EMPTY_FIELD
+import ckan.model as model
 
 uuidMap = {}
+licenseMap = {}
 
 
 def generate_uuid(key=None):
@@ -45,6 +47,21 @@ def to_bool(val: str):
         return "true"
     elif val.lower() == 'no':
         return "false"
+    
+def mapFromCKANLicense(val: str):
+    # val counld be license_id or license_url
+    register = model.Package.get_license_register()
+    if len(licenseMap) == 0:
+        for license_id, license in register.items():
+            licenseMap[license_id] = license.url
+    if licenseMap.get(val):
+        return [{"@id": licenseMap[val]}]
+    elif (val.startswith("http")):
+        return [{"@id": val}]
+    else:
+        # CKAN license that does not have url
+        return [{"@id": f"http://data.urbandatacentre.ca/licenses/{val}"}]
+        
 
 
 # def to_datetime(val: str):
@@ -62,4 +79,5 @@ all_helpers = {
     "to_date": to_date,
     "to_bool": to_bool,
     "split_to_uris": split_to_uris,
+    "mapFromCKANLicense": mapFromCKANLicense,
 }
